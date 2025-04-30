0b60929959b897b2c8bd737631b5e92e2ab9c423
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-04-30 17:29:40 -0400

    Starting login form

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+import { getAuth, signInWithCustomToken } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,28 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+function attemptLogin() {
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  if (username == '' || password == '') {
+    return false;
+  }
+
+  const auth = getAuth();
+  signInWithCustomToken(auth, token)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    return true;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+
+}
