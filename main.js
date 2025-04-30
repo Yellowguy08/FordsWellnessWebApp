ecde00cdd2940e1f9ac2be628265705f64da0966
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-04-30 17:36:54 -0400

    Testing random stuff

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
-import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { getAuth, signInWithCustomToken } from "/firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
