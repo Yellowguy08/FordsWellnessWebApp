e7ed0a1cc99fbda934e68b7a701c6b68d30decfc
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-04-30 22:13:48 -0400

    testing more firebase stuff

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
-import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+// import { getFireStore} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+
+const firebaseApp = initializeApp  ({
+
   apiKey: "AIzaSyAsZMvBjivVPnEff1-hvkpgv6ubIVOErGk",
   authDomain: "fordswellness-77af3.firebaseapp.com",
   projectId: "fordswellness-77af3",
@@ -16,33 +18,29 @@ const firebaseConfig = {
   messagingSenderId: "757264159793",
   appId: "1:757264159793:web:899734af389db35adf7110",
   measurementId: "G-LN5XMD3LXP"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+})
 
-function attemptLogin() {
-  
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  
-  if (username == '' || password == '') {
-    return false;
+const auth = getAuth(firebaseApp);
+// const db = getFirestore(firebaseApp);
+
+onAuthStateChanged(auth, user => {
+
+  if (user != null) {
+    console.log('logged in!');
+  } else {
+    console.log('No user');
   }
 
-  const auth = getAuth();
-  signInWithCustomToken(auth, token)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    return true;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
+});
 
-}
+// function attemptLogin() {
+  
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+  
+//   if (username == '' || password == '') {
+//     return false;
+//   }
+
+// }
