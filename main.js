9d4a8c75339061ba7b67a90df579a427035be6d8
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-05-07 14:08:54 -0400

    Tutorial 2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword  } from "https:
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseApp = initializeApp  ({
+const firebaseConfig = {
 
   apiKey: "AIzaSyAsZMvBjivVPnEff1-hvkpgv6ubIVOErGk",
   authDomain: "fordswellness-77af3.firebaseapp.com",
@@ -19,78 +19,68 @@ const firebaseApp = initializeApp  ({
   appId: "1:757264159793:web:899734af389db35adf7110",
   measurementId: "G-LN5XMD3LXP"
 
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app);
+
+const button = document.getElementById("login");
+button.addEventListener("click", function (event) {
+  event.preventDefault()
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage)
+  });
 })
 
-const auth = getAuth(firebaseApp);
-// const db = getFirestore(firebaseApp);
+// function createActivities() {
 
-onAuthStateChanged(auth, user => {
+//     let activity_box = document.createElement("div");
+//     activity_box.classList.add("activity-box");
 
-  if (user != null) {
-    console.log('logged in!');
-  } else {
-    console.log('No user');
-  }
+//     let activity_info = document.createElement("div");
+//     activity_info.classList.add("activity-info");
 
-});
-
-function attemptLogin() {
-  
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  
-  const submit = document.getElementById("login");
-
-  submit.addEventListener("click", function (event) {
-    event.preventDefault();
-    alert(5);
-  })
-
-  if (username == "test1@email.com" && password == "123456") {
-    return true;
-  } else {
-    return false;
-  }
-
-}
-
-function createActivities() {
-
-    let activity_box = document.createElement("div");
-    activity_box.classList.add("activity-box");
-
-    let activity_info = document.createElement("div");
-    activity_info.classList.add("activity-info");
-
-    let activity_name = document.createElement("span");
-    activity_name.appendChild(document.createTextNode("Nap Time"))
+//     let activity_name = document.createElement("span");
+//     activity_name.appendChild(document.createTextNode("Nap Time"))
     
-    let activity_room = document.createElement("span");
-    activity_room.appendChild(document.createTextNode("128"))
+//     let activity_room = document.createElement("span");
+//     activity_room.appendChild(document.createTextNode("128"))
 
-    activity_info.appendChild(activity_name);
-    activity_info.appendChild(activity_room);
+//     activity_info.appendChild(activity_name);
+//     activity_info.appendChild(activity_room);
 
-    let teacher_box = document.createElement("div");
+//     let teacher_box = document.createElement("div");
     
-    let activity_teacher = document.createElement("span");
-    activity_teacher.classList.add("activity-teacher");
-    activity_teacher.appendChild(document.createTextNode("Mr."));
+//     let activity_teacher = document.createElement("span");
+//     activity_teacher.classList.add("activity-teacher");
+//     activity_teacher.appendChild(document.createTextNode("Mr."));
 
-    teacher_box.appendChild(activity_teacher);
+//     teacher_box.appendChild(activity_teacher);
 
-    let max_capacity = document.createElement("div");
-    max_capacity.classList.add("capacity-bar");
-    max_capacity.classList.add("max-capacity");
+//     let max_capacity = document.createElement("div");
+//     max_capacity.classList.add("capacity-bar");
+//     max_capacity.classList.add("max-capacity");
 
-    let curr_capacity = document.createElement("div");
-    curr_capacity.classList.add("capacity-bar");
-    curr_capacity.classList.add("curr-capacity");
+//     let curr_capacity = document.createElement("div");
+//     curr_capacity.classList.add("capacity-bar");
+//     curr_capacity.classList.add("curr-capacity");
 
-    activity_box.appendChild(activity_info);
-    activity_box.appendChild(teacher_box);
-    activity_box.appendChild(max_capacity);
-    activity_box.appendChild(curr_capacity);
+//     activity_box.appendChild(activity_info);
+//     activity_box.appendChild(teacher_box);
+//     activity_box.appendChild(max_capacity);
+//     activity_box.appendChild(curr_capacity);
 
-    document.getElementById("body").appendChild(activity_box);
-}
+//     document.getElementById("body").appendChild(activity_box);
+// }
